6e72bdbad93476c90f236371157ed5fb8ac79237
Sarah Meyer <Smeyer@Sarahs-MacBook-Pro.local>
2017-05-22 09:09:51 -0600

    discovery_query complete

---
+++ discovery/discovery_query.py
@@ -0,0 +1,39 @@
+from watson_developer_cloud import DiscoveryV1
+import sys
+import json
+import time
+import argparse
+import pandas as pd
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+def read_json_file(file_path):
+    with open(file_path) as json_file:
+        json_content = json_file.read()
+        json_data = json.loads(json_content)
+    return(json_data)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_file")
+    args = parser.parse_args()
+
+    dotenv_path = join(dirname(__file__), '.gitignore')
+    load_dotenv(dotenv_path)
+
+    query_json = read_json_file(args.query_file)
+
+    discovery = DiscoveryV1(
+        username=os.environ.get("DISCOVERY_USERNAME"),
+        password=os.environ.get)("DISCOVERY_PASSWORD"),
+        version="2016-12-01"
+    )
+
+    collection_id = os.environ.get('DISCOVERY_COLLECTION_ID')
+    configuration_id = os.environ.get('DISCOVERY_CONFIGURATION_ID')
+    environment_id = os.environ.get('DISCOVERY_ENVIRONMENT_ID')
+
+    response = discovery.query(environment_id,
+                               collection_id,
+                               query_json)
+    display_results(response)
